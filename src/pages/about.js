1354e0d5f197b69467bbd37c1dd344d57677ee3e
Alessia Piscopo <alessia.piscopo@hotmail.com>
2022-07-07 01:16:25 -0400

    Create about and projects page

---
+++ src/pages/about.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+function About() {
+  return (
+    <div>
+      <h1 className="about">About Me</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias eum
+        laboriosam corrupti deleniti! Beatae quas adipisci aliquam iste nesciunt
+        vitae. Totam beatae nesciunt perferendis inventore temporibus sequi
+        facere saepe amet?
+      </p>
+    </div>
+  )
+}
+
+export default About
---
+++ src/pages/projects/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function Projects() {
+  return (
+    <div>
+      <h1>Projects</h1>
+      <h2>Some of my projects</h2>
+    </div>
+  )
+}
+
+export default Projects
