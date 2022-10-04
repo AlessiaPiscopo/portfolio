f73c5aa4004f416ef55ffbbddacdc8793893400c
Alessia Piscopo <alessia.piscopo@hotmail.com>
2022-10-04 13:38:50 -0400

    Create background image component to reuse in various app sections

---
+++ src/components/BackgroundTexture.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { StyledBackgroundTexture } from "../styles/BackgroundTexture.styled";
+
+export default function BackgroundTexture() {
+  return <StyledBackgroundTexture></StyledBackgroundTexture>;
+}
