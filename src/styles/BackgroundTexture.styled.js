f287b3f37967d755be1b2acdf3ed690777b9a884
Alessia Piscopo <alessia.piscopo@hotmail.com>
2022-10-04 13:39:49 -0400

    Style background image component

---
+++ src/styles/BackgroundTexture.styled.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const StyledBackgroundTexture = styled.div`
+  width: 100%;
+  height: 100%;
+  /* background-image: url("/texture-gray.jpeg"); */
+  position: absolute;
+  left: 0;
+  z-index: -1;
+  filter: brightness(120%) grayscale(100%);
+
+  opacity: 0.25;
+`;
